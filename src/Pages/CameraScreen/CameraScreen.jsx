d1489efadddad371a0f2c6ebccde051639b7bdc6
Joginder Yadav <joginder@indicosmic.com>
2024-04-29 18:12:53 +0530

    removed

--- src/Pages/CameraScreen/CameraScreen.jsx
+++ src/Pages/CameraScreen/CameraScreen.jsx
@@ -8,7 +8,7 @@ const CameraScreen = () => {
 
   useEffect(() => {
     // Initialize webcam instance
-    webcamInstance.current = new Webcam(webcamRef.current, "Environment");
+    webcamInstance.current = new Webcam(webcamRef.current);
 
     // Start webcam
     webcamInstance.current
