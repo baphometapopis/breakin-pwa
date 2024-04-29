39c2d3a492a8def5b0ee8452c80c598715aac70e
Joginder Yadav <joginder@indicosmic.com>
2024-04-29 12:15:34 +0530

    added old code

--- src/Pages/CameraScreen/CameraScreen.jsx
+++ src/Pages/CameraScreen/CameraScreen.jsx
@@ -1,117 +1,141 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import React, { useState, useEffect, useRef } from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "react-html5-camera-photo/build/css/index.css";
+import { Camera } from "react-html5-camera-photo";
+import "./CameraScreen.css"; // Import the CSS file
 
 const CameraScreen = () => {
-  const [stream, setStream] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(true);
   const [capturedImage, setCapturedImage] = useState(null);
-  const videoRef = useRef(null);
+  const [allCapturedImages, setAllCapturedImages] = useState([]);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const navigation = useNavigate();
 
-  useEffect(() => {
-    const constraints = { video: { facingMode: "environment" } }; // Use rear-facing camera by default
+  const handleTakePhoto = (dataUri) => {
+    setCapturedImage(dataUri);
+  };
 
-    const initializeCamera = async () => {
-      try {
-        const stream = await navigator.mediaDevices.getUserMedia(constraints);
-        setStream(stream);
-        if (videoRef.current) {
-          videoRef.current.srcObject = stream;
-        }
-      } catch (err) {
-        console.error("Error accessing the camera:", err);
-      }
-    };
+  const handleRetakePhoto = () => {
+    setCapturedImage(null);
+  };
 
-    initializeCamera();
+  const handleSavePhoto = () => {
+    if (capturedImage) {
+      const data = {
+        images: capturedImage,
+        imagename: images[currentImageIndex]?.name,
+      };
+      setAllCapturedImages([...allCapturedImages, data]);
 
-    // Cleanup function
-    return () => {
-      if (stream) {
-        stream.getTracks().forEach((track) => {
-          track.stop();
+      if (currentImageIndex < images.length - 1) {
+        setCurrentImageIndex(currentImageIndex + 1);
+        setCapturedImage(null);
+        setIsModalOpen(true);
+      } else {
+        navigation("/ShowInspectionImages", {
+          state: {
+            capturedImagesWithOverlay: allCapturedImages,
+            proposalInfo: "info",
+          },
         });
       }
-    };
-  }, []);
-
-  // Add event listener to handle orientation change
-  useEffect(() => {
-    const handleOrientationChange = () => {
-      if (videoRef.current) {
-        const angle = window.orientation || 0;
-        videoRef.current.style.transform = `rotate(${angle}deg)`;
-      }
-    };
-
-    window.addEventListener("orientationchange", handleOrientationChange);
-
-    return () => {
-      window.removeEventListener("orientationchange", handleOrientationChange);
-    };
-  }, []);
-
-  const handleCapture = () => {
-    if (videoRef.current) {
-      const canvas = document.createElement("canvas");
-      const context = canvas.getContext("2d");
-      canvas.width = videoRef.current.videoWidth;
-      canvas.height = videoRef.current.videoHeight;
-      context.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
-      const capturedImageData = canvas.toDataURL("image/png");
-      setCapturedImage(capturedImageData);
     }
   };
 
+  const images = [
+    {
+      id: "1",
+      name: "Odometer with Engine on Position",
+      is_mand: "1",
+      sample_image_url:
+        "https://bp.mypolicynow.com/api/images/breakin_sample_image/ODOMETER.jpeg",
+    },
+    {
+      id: "2",
+      name: "Windscreen Inside to Outside",
+      is_mand: "1",
+      sample_image_url:
+        "https://bp.mypolicynow.com/api/images/breakin_sample_image/Windscreen-Inside-to-Outside.jpg",
+    },
+    {
+      id: "3",
+      name: "Windscreen Outside to Inside",
+      is_mand: "1",
+      sample_image_url:
+        "https://bp.mypolicynow.com/api/images/breakin_sample_image/Windscreen-Outside-to-Inside.jpg",
+    },
+  ];
+
+  useEffect(() => {}, [isModalOpen]);
   return (
-    <div
-      style={{
-        backgroundColor: "#F1FBFF",
-        position: "relative",
-        height: "100vh",
-      }}
-    >
-      <video
-        ref={videoRef}
-        autoPlay
-        playsInline
-        style={{
-          maxWidth: "100%",
-          maxHeight: "100%",
-          width: "auto",
-          height: "auto",
-        }}
-      />
-      <button
-        onClick={handleCapture}
-        style={{
-          position: "absolute",
-          bottom: "20px",
-          left: "50%",
-          transform: "translateX(-50%)",
-          padding: "10px 20px",
-          fontSize: "16px",
-          borderRadius: "5px",
-          backgroundColor: "white",
-          color: "black",
-          border: "none",
-          cursor: "pointer",
-        }}
-      >
-        Capture
-      </button>
+    <div className="camera-container">
+      {isModalOpen && (
+        <div className="modal">
+          <div style={{ flex: 0.4 }}>
+            <img
+              src={images[currentImageIndex]?.sample_image_url}
+              alt={images[currentImageIndex]?.name}
+              style={{ width: "100%", height: "80%" }}
+            />
+            <p className="modalText">{images[currentImageIndex]?.name}</p>
+            <p className="modalText">
+              {currentImageIndex + 1}/{images.length}
+            </p>
+          </div>
+          <div style={{ flex: 0.6 }}>
+            <h5 className="modalText">
+              Please follow the instructions to capture Image
+            </h5>
+
+            <p className="instructionText">
+              {"\u2022"} The Image has to be captured during the daylight. Image
+              captured in basements or shades (ex. tree shades) will not be
+              valid.
+            </p>
+            <p className="instructionText">
+              {"\u2022"} Please keep your car Turned ON for 10 seconds and then
+              Start Taking Images
+            </p>
+            <p className="instructionText">
+              {"\u2022"} A Reference Image is placed in the Middle of the Camera
+              while capturing Image
+            </p>
+            <p className="instructionText">
+              {"\u2022"} Click on Ok When Your are Ready
+            </p>
+
+            <div onClick={() => setIsModalOpen(false)} className="ok-button">
+              Start Camera
+            </div>
+          </div>
+        </div>
+      )}
+      {!isModalOpen && !capturedImage && (
+        <div>
+          <Camera
+            onTakePhoto={(dataUri) => handleTakePhoto(dataUri)}
+            idealFacingMode="environment"
+            isMaxResolution={true}
+            imageType="jpg"
+            isFullscreen={true}
+            imageCompression={0.97}
+            isImageMirror={true}
+          />
+        </div>
+      )}
       {capturedImage && (
-        <img
-          src={capturedImage}
-          alt="Captured"
-          style={{
-            position: "absolute",
-            bottom: "80px",
-            left: "50%",
-            transform: "translateX(-50%)",
-            maxWidth: "80%",
-            maxHeight: "80%",
-            boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.3)",
-          }}
-        />
+        <div>
+          <img src={capturedImage} alt="Captured" className="captured-image" />
+
+          <div className="save-button-container">
+            <button className="save-button" onClick={handleSavePhoto}>
+              Save
+            </button>
+            <button className="retake-button" onClick={handleRetakePhoto}>
+              Retake
+            </button>
+          </div>
+        </div>
       )}
     </div>
   );
