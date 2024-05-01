8b5b2d031de2dcc1bec723a5b7aa53288abceb7c
Joginder Yadav <joginder@indicosmic.com>
2024-05-01 14:18:44 +0530

    new camera model

--- src/App.js
+++ src/App.js
@@ -1,50 +1,31 @@
-import React from "react";
-import "./App.css";
-import Camera from "./Pages/CameraScreen/CameraScreen";
+// App.js
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import HomePage from "./Pages/Home/HomePage";
+import CameraScreen from "./Pages/CameraScreen/CameraScreen";
+import ProposalInfoPage from "./Pages/ProposalInfoPage/ProposalInfoPage";
+import ShowinspectionImages from "./Pages/ShowMandatoryInspectionimages/ShowMandatoryInspectionimages";
+import VideoRecorder from "./Pages/VideoRecord";
+import VideoPreview from "./Pages/VideoPreview";
 
 function App() {
   return (
-    <div className="App">
-      {/* <header className="App-header">
-        <h1>React Camera App</h1>
-      </header> */}
-      <main>
-        <Camera />
-      </main>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route
+          path="/proposal-info/:proposalNumber"
+          element={<ProposalInfoPage />}
+        />
+        <Route path="/camera" element={<CameraScreen />} />
+        <Route
+          path="/ShowInspectionImages"
+          element={<ShowinspectionImages />}
+        />
+        <Route path="/VideoRecord" element={<VideoRecorder />} />
+        <Route path="/VideoPreview" element={<VideoPreview />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
-// // App.js
-// import { Routes, Route, BrowserRouter } from "react-router-dom";
-// import HomePage from "./Pages/Home/HomePage";
-// import CameraScreen from "./Pages/CameraScreen/CameraScreen";
-// import ProposalInfoPage from "./Pages/ProposalInfoPage/ProposalInfoPage";
-// import ShowinspectionImages from "./Pages/ShowMandatoryInspectionimages/ShowMandatoryInspectionimages";
-// import VideoRecorder from "./Pages/VideoRecord";
-// import VideoPreview from "./Pages/VideoPreview";
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path="/" element={<HomePage />} />
-//         <Route
-//           path="/proposal-info/:proposalNumber"
-//           element={<ProposalInfoPage />}
-//         />
-//         <Route path="/camera" element={<CameraScreen />} />
-//         <Route
-//           path="/ShowInspectionImages"
-//           element={<ShowinspectionImages />}
-//         />
-//         <Route path="/VideoRecord" element={<VideoRecorder />} />
-//         <Route path="/VideoPreview" element={<VideoPreview />} />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
